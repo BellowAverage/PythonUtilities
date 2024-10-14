91a772b468ed8f8cdb0db83dc67fd08b08a4338d
Chris Wang <chriswang2025@u.northwestern.edu>
2024-10-14 16:39:20 -0500

    1014

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='BellowAveragePythonUtilities',
     version='0.1.0',
-    packages=['OpenAIAPI'],
+    packages=['conversation'],
     install_requires=["openai==0.28"],
     description='A simple API for Python APIs',
     long_description=open('README.md').read(),
